ffc3d2741922a473e22c302c1a7dd4b0e9636f57
MatiasAbregu <matiasabregu506@gmail.com>
2026-07-06 00:15:48 -0300

    Diary 2 finished

--- src/books/Diary2.jsx
+++ src/books/Diary2.jsx
@@ -183,6 +183,7 @@ export const Diary2 = ({ setDiarySelected }) => {
                         </div>
                     </Page>
 
+                    {/* CAPITULO 9 */}
                     <Page title="9 ~ El conejo y la tortuga..." number="11" pageClass={"pageDiary2 page-interior"}>
                         <p>
                             &emsp; Después de un buen tiempo... Retomo el diario que dije escribir cuando creyera que era necesario...  <br /> &emsp; Y acá me encuentro, para narrar 6 capítulos; pero primero, narrar el que tenía pendiente. Y es el del conejo y la tortuga, o más bien...
@@ -232,14 +233,249 @@ export const Diary2 = ({ setDiarySelected }) => {
                             <img src={images.TogetherThinkingAboutTheOther} style={{ width: "85%" }} />
                         </div>
                     </Page>
+
+                    {/* CAPITULO 10 */}
                     <Page title="10 ~ El fondo..." number="15" pageClass={"pageDiary2 page-interior"}>
                         <p>
                             &emsp; Sin duda, este diario va a quedar para la anécdota de la cantidad de adversidades que han aparecido en este mes... Pero de la que vamos a hablar ahora creo que ha sido la más chocante o profunda... La que realmente nos ha forjado como pareja para que podamos ser más fuerte y entendernos muchísimo mejor. Quién dice que de la caída uno se levanta más fuerte, no podría tener más razón. O que hay que tocar fondo para poder saber hasta donde uno puede presionar tristemente...
                         </p>
+                        <audio src={audios.CallYourName} controls style={{ alignSelf: "center", marginTop: "50px" }} />
                     </Page>
                     <Page number="16" pageClass={"pageDiary2 page-interior"}>
-
+                        <p style={{ marginTop: "10px" }}>
+                            &emsp; Esa tarde/noche sin duda ninguno de los dos la va a olvidar. A pesar de haber derramado ambos muchas lagrimas y sufrir bastante (donde la adversidad casi nos gana) decidimos volver a elegirnos como siempre lo hacemos. Los pilares de nuestra relación nos volvieron a levantar para continuar juntos...
+                            <h3 style={{ textAlign: "center", margin: "15px 0" }}>
+                                <i>🐢 Honestidad 🐇</i>
+                            </h3>
+                            <h3 style={{ textAlign: "center", margin: "15px 0" }}>
+                                <i>🐢 Empatía 🐇</i>
+                            </h3>
+                            <h3 style={{ textAlign: "center", margin: "15px 0" }}>
+                                <i>🐢 Paciencia 🐇</i>
+                            </h3>
+                        </p>
+                        <p>
+                            &emsp; Y porque sí, como dije, la adversidad casi nos gana pero eso jamás sucederá... Porque ambos vamos a ganar esta guerra dónde todo se pone en contra de nuestro amor.
+                        </p>
+                        <h3 style={{ textAlign: "center", margin: "20px 0" }}>
+                            <i>🐢 Nada ni nadie nos ganará si permanecemos juntos. 🐇</i>
+                        </h3>
                     </Page>
+                    <Page number="17" pageClass={"pageDiary2 page-interior"}>
+                        <p style={{ marginTop: "10px" }}>
+                            &emsp; Y obviamente, no puede faltar como en ningún capítulo, algunas palabras de aliento de mi hermosa novia que son las que me dan fuerzas en el día a día para no caer en la locura nuevamente.
+                        </p>
+                        <div className='sheSupportingMe'>
+                            <div className='winter-frame'>
+                                <img src={images.Deepdown1} />
+                            </div>
+                            <div className='winter-frame'>
+                                <img src={images.Deepdown2} />
+                            </div>
+                            <div className='winter-frame'>
+                                <img src={images.Deepdown3} />
+                            </div>
+                        </div>
+                    </Page>
+
+                    {/* CAPITULO 11 */}
+                    <Page title="11 ~ El caballero antiheróe" number="18" pageClass={"pageDiary2 page-interior"}>
+                        <p>
+                            &emsp; Quién diría que el sol y la luna se unirían para terminar de conformar mi personalidad... Cuando daba por muerto mi lado de la luna, esta vuelve a aparecer pero más fuerte y más concentrada. Y esta vez para quedarse y nunca más irse.
+                        </p>
+                        <audio src={audios.TheBreathOfAVow} controls style={{ alignSelf: "center", margin: "20px 0" }} />
+                        <p>
+                            &emsp; ¿Y por qué vuelve? Vuelve porque no pude soportar la noche que me dijiste que lloraste, suplicando, por querer conocerme y aún así, te trataron con indiferencia y desprecio, a vos y a nuestros bellos recuerdos; como si no valieran nada. Sin duda ese día me queda marcado por el resto de mis días.
+                        </p>
+                        <h3 style={{ textAlign: "center", margin: "20px 0" }}>
+                            <i>🐢 Quiero cuidarte de todo, te lo prometí. 🐇</i>
+                        </h3>
+                    </Page>
+                    <Page number="19" pageClass={"pageDiary2 page-interior"}>
+                        <p style={{ marginTop: "10px" }}>
+                            &emsp; Por lo que el sol y la luna se unen para terminar de conformar mi personalidad... Cuando daba por muerto mi lado de la luna, vuelve a aparecer pero más fuerte y más concentrada. Y esta vez para quedarse y nunca más irse.
+                        </p>
+                        <div className='sunAndMoon'>
+                            <div className='winter-frame'>
+                                <img src={images.SunAndMoon} />
+                            </div>
+                        </div>
+                        <h3 style={{ textAlign: "center", margin: "20px 0" }}>
+                            <i>🐢&emsp; Así que, que venga cualquier adversidad que la voy a superar con tal de no ver una lagrima en ese bello rostro. 🐇</i>
+                        </h3>
+                    </Page>
+
+                    {/* FUNNY TIME 1 */}
+                    <Page title="~ Funny Time (1)" number="20" pageClass={"pageDiary2 page-interior"}>
+                        <p>
+                            &emsp; Este es un nuevo apartado que aparece a partir de este diario. Consistirá en momentos graciosos que hemos ido vivivendo y no he encontrado lugar para ponerlos en un capítulo.
+                        </p>
+                        <div className='funnyTime1'>
+                            <div className='winter-frame'>
+                                <img src={images.CabyHdp} />
+                            </div>
+                            <div className='winter-frame'>
+                                <img src={images.DeadOrLive} />
+                            </div>
+                            <div className='winter-frame'>
+                                <img src={images.HouseTree} />
+                            </div>
+                            <div className='winter-frame'>
+                                <img src={images.ImOld} />
+                            </div>
+                        </div>
+                    </Page>
+                    <Page number="21" pageClass={"pageDiary2 page-interior"}>
+                        <div className='funnyTime1-2'>
+                            <div className='winter-frame'>
+                                <img src={images.InstagramMistake} />
+                            </div>
+                            <div className='winter-frame'>
+                                <img src={images.SheAuthorizedUsToDestroyTheHouse} />
+                            </div>
+                            <div className='winter-frame'>
+                                <img src={images.SheDoesntThinkTooMuch} />
+                            </div>
+                            <div className='winter-frame'>
+                                <img src={images.YoWhatTheHell} />
+                            </div>
+                        </div>
+                    </Page>
+                    <Page number="22" pageClass={"pageDiary2 page-interior"}>
+                        <div className='funnyTime1-3'>
+                            <div className='winter-frame'>
+                                <img src={images.TalkingWithHerBrother1} />
+                            </div>
+                            <div className='winter-frame'>
+                                <img src={images.TalkingWithHerBrother2} />
+                            </div>
+                        </div>
+                    </Page>
+
+                    {/* CAPITULO 12 */}
+                    <Page title="12 ~ Los acertijos" number="23" pageClass={"pageDiary2 page-interior"}>
+                        <audio src={audios.Tranquility} controls style={{ alignSelf: "center", marginTop: "10px" }} />
+                        <p>
+                            &emsp; ¿Qué curioso no? Así fue como nos conocimos, vos siendo una detective de los acertijos de mi vida. No me di cuenta como pasó el tiempo pero has ido cada día aprendiendo más y más sobre mí. <br />
+                            &emsp; Solo espero que todo lo que he ido proponiéndote te sirva en algún momento.
+                        </p>
+                        <i style={{ textAlign: "center", fontSize: "1.2rem", margin: "15px 0" }}>¿Qué es un acertijo?</i>
+                        <div className='puzzleDiv'>
+                            <div className='winter-frame'>
+                                <img src={images.Puzzle} />
+                            </div>
+                        </div>
+                    </Page>
+                    <Page number="24" pageClass={"pageDiary2 page-interior"}>
+                        <p style={{ marginTop: "10px" }}>
+                            &emsp; Resulta que todos estos acertijos es para descubrir que personas se toman en serio la vida, los desafios que pueden encontrarse o no... <br />
+                            &emsp; Para mí sorpresa llegaste vos (sea por tus decisiones tomadas por vos, Dios o el universo). Una hermosa personita que se tomó el tiempo de conocerme y aumentar sus conocimientos. Al fin y al cabo: "una vida sin saber es una vida aburrida". <br />
+                            &emsp; Por eso quiero decirte...
+                        </p>
+                        <i style={{ textAlign: "center", fontSize: "1.2rem", margin: "15px 0" }}>Gracias</i>
+                        <p>
+                            &emsp; Gracias por tomarte el tiempo de querer conocer mi mundo interno cada día más y más pese tener nuestras diferencias ideológicas. Todo lo que hemos ido hablando te va a ayudar tarde o temprano, después de todo, a mi me han servido a lo largo de mi vida. Entonces ¿Qué es un puzzle para Matías? No es más que un pedacito de su vida, un pedacito de su saber para resolver problemas o adversidades, entender más el mundo en el que vivimos.
+                        </p>
+                    </Page>
+
+                    {/* CAPITULO 13 */}
+                    <Page title="13 ~ La bella flor que duda de si" number="25" pageClass={"pageDiary2 page-interior"}>
+                        <p>
+                            (Breve consejo: la canción del anterior capítulo también es para este) <br />
+                            &emsp; Nos vamos acercando al final del diario... Y creo que es conveniente mencionar esto porque es increíble como aún te cueste aceptarte. Hay algo que me preocupa mucho en la relación y es precisamente que no te ames, al no amarte y no mostrarte terminas lastimándome. Lo dije en el otro diario pero lo voy a repetir hasta que se te grabe:
+                        </p>
+                        <h3 style={{ textAlign: "center", margin: "20px 0" }}>
+                            <i>🐢 Andrea, sos hermosa, una flor bella que no aprecia lo linda que es en foto, en video, en persona o en VIDEOLLAMADA. 🐇</i>
+                        </h3>
+                        <p>
+                            &emsp; Por eso, este capítulo es un pedido de auxilio a que te aceptes por el bien de la relación. Por mi felicidad, la que tanto juraste cuidar y proteger.
+                        </p>
+                    </Page>
+                    <Page number="26" pageClass={"pageDiary2 page-interior"}>
+                        <div className='funnyTime1-3'>
+                            <div className='winter-frame'>
+                                <img src={images.ProtectMe1} />
+                            </div>
+                            <div className='winter-frame'>
+                                <img src={images.ProtectMe2} />
+                            </div>
+                        </div>
+                    </Page>
+                    <Page number="27" pageClass={"pageDiary2 page-interior"}>
+                        <p style={{ marginTop: "10px" }}>
+                            &emsp; Quisiera con todas mis fuerzas poder entregarte mi visión para que puedas observar lo que yo observo cuando te veo, mi corazón para que sientas lo que siento cuando te veo y mi "amor" para que puedas ver todo lo que te amo. <br />
+                            &emsp;Porque sí, esta mujer, es la que tiene mi corazón:
+                        </p>
+                        <div className='myFlower'>
+                            <div className='winter-frame'>
+                                <video controls src={videos.Flower1} />
+                            </div>
+                            <div className='winter-frame'>
+                                <img src={images.Flower2} />
+                            </div>
+                            <div className='winter-frame'>
+                                <img src={images.Flower3} />
+                            </div>
+                            <div className='winter-frame'>
+                                <img src={images.Flower4} />
+                            </div>
+                        </div>
+                    </Page>
+
+                    {/* CAPITULO 14 */}
+                    <Page title="14 ~ ¡Por fin!" number="28" pageClass={"pageDiary2 page-interior"}>
+                        <p>
+                            &emsp; Bueno mi amor... Hemos llegado al final de este hermoso diario y ya en este capítulo me comienzo a emocionar jajaja... ¡Qué título tan llamativo! Si me haces el honor, me gustaría que vuelvas a la introducción y leas lo que dice, antes de continuar leyendo este capítulo. <br />
+                            &emsp; Asumiendo que lo has leído entonces...
+                        </p>
+                        <h3 style={{ textAlign: "center", margin: "20px 0" }}>
+                            <i>🐢 ¡LO HICIMOS, NOS CONOCIMOS EN PERSONA! 🐇</i>
+                        </h3>
+                        <p>
+                            &emsp; Después de tanta lucha logramos conocernos (no de la forma que nos hubiese gustado pero lo hicimos y es lo que vale). Jamás me voy a olvidar del jueves 02/06/2026, fallé el lunes de esa semana y rogué por otra oportunidad para poder conocerte... Y contra todo pronostico... Se nos dió, logramos vernos y poder dar nuestro primer abrazo así como compartir un hermoso momento donde sostuvimos nuestras manos.
+                        </p>
+                    </Page>
+                    <Page number="29" pageClass={"pageDiary2 page-interior"}>
+                        <p style={{ marginTop: "10px" }}>
+                            &emsp; Y ese día, cuando comenzaste a mostrar dudas me comencé a preocupar hasta que recordé lo que te juré, como dije el lunes...
+                        </p>
+                        <audio src={audios.Freedom} controls style={{ alignSelf: "center", marginTop: "10px" }} />
+                        <p>
+                            &emsp; Esa es la canción que me acompañó todo el jueves "Counter Attack-Mankind". Una canción que me dió la fuerza que necesitaba para no perder ante el miedo ni ante nada. Te juré que te vería y juré que si tenía otra oportunidad el jueves para verte, iría sino me quitaría la vida. <br />
+                            &emsp; Por eso ese día estuve tan sereno y concentrado, aunque la duda me invadió en un momento, peleé, peleé contra el miedo de ser descubierto, contra el miedo de no poder verte de nuevo por culpa de tus papás (sobre todo tu mamá, la gran enemiga de esta relación y la que no nos permite vernos) y contra el miedo de que pierdas la confianza que tenés en mí. Prometí cuidarte, y eso haré hasta donde vos me lo permitas, mi bella y dulce princesa ángelical.
+                        </p>
+                    </Page>
+                    <Page number="30" pageClass={"pageDiary2 page-interior"}>
+                        <p style={{ marginTop: "10px" }}>
+                            &emsp; Gracias por todos tus regalos ese día, gracias por dejarte abrazar, gracias por querer abrazarme y sostener mi mano pese a que estuvieras hecha una bomba de nervios (cosita hermosa jajaja). Y que consté...
+                        </p>
+                        <h3 style={{ textAlign: "center", margin: "20px 0" }}>
+                            <i>🐢 ¡ESTA GUERRA LA GANAMOS NOSOTROS! 🐇</i>
+                        </h3>
+                        <div className='thisWar'>
+                            <div className='winter-frame'>
+                                <img src={images.ThisWarWeAreGoingToWin} />
+                            </div>
+                        </div>
+                    </Page>
+                    <Page number="31" pageClass={"pageDiary2 page-interior"}>
+                        <p style={{ marginTop: "10px" }}>
+                            &emsp; Porque juntos, somos más fuertes y nada ni nadie detendrá nuestro amor. Un amor que pese a sus caídas y subidas, pese a la adversidad y a los problema sigue brillando como el día que comenzó, es más...
+                        </p>
+                        <h3 style={{ textAlign: "center", margin: "20px 0" }}>
+                            <i>Brilla aún más. Hasta el 20 de julio, mi bella dama, donde nos veremos nuevamente.</i>
+                        </h3>
+                        <div className='firstDate'>
+                            <div className='winter-frame'>
+                                <img src={images.MyGift} />
+                            </div>
+                            <div className='winter-frame'>
+                                <img src={images.HerGift} />
+                            </div>
+                        </div>
+                    </Page>
+
+                    <Page title="Continuará..." number="32" pageClass={"pageDiary2 page-interior"}></Page>
 
                     <Page isCover={true} pageClass={"pageDiary2 book-cover"}>
                     </Page>
@@ -253,10 +489,3 @@ export const Diary2 = ({ setDiarySelected }) => {
         </>
     )
 }
-
-/* 
-El caballero antiheróe 
-Los acertijos 
-El rechazo (familia)
-La bella flor que duda de si 
-*/
--- src/diary-files/diary2/Diary2-Data.js
+++ src/diary-files/diary2/Diary2-Data.js
@@ -20,9 +20,40 @@ import Turtle from './imgs/MyLittleTurtle.png';
 import FirstTurtle from './imgs/FirstTurtle.jpeg';
 import SecondTurtle from './imgs/SecondTurtle.jpeg';
 import TogetherThinkingAboutTheOther from './imgs/TogetherThinkingAboutTheOther.jpeg';
+import Deepdown1 from './imgs/Deepdown1.jpeg';
+import Deepdown2 from './imgs/Deepdown2.jpeg';
+import Deepdown3 from './imgs/Deepdown3.jpeg';
+import SunAndMoon from './imgs/SunAndMoon.jpeg';
+import CabyHdp from './imgs/CabyHdp.jpeg';
+import DeadOrLive from './imgs/DeadOrLive.jpeg';
+import HouseTree from './imgs/HouseTree.jpeg';
+import ImOld from './imgs/ImOld.jpeg';
+import InstagramMistake from './imgs/InstagramMistake.jpeg';
+import SheAuthorizedUsToDestroyTheHouse from './imgs/SheAuthorizedUsToDestroyTheHouse.jpeg';
+import SheDoesntThinkTooMuch from './imgs/SheDoesntThinkTooMuch.jpeg';
+import TalkingWithHerBrother1 from './imgs/TalkingWithHerBrother1.jpeg';
+import TalkingWithHerBrother2 from './imgs/TalkingWithHerBrother2.jpeg';
+import YoWhatTheHell from './imgs/YoWhatTheHell.jpeg';
+import Puzzle from './imgs/Puzzle.png';
+import ProtectMe1 from './imgs/ProtectMe1.jpeg';
+import ProtectMe2 from './imgs/ProtectMe2.jpeg';
+
+import Flower1 from './videos/Flower1.mp4';
+import Flower2 from './imgs/Flower2.jpeg';
+import Flower3 from './imgs/Flower3.jpeg';
+import Flower4 from './imgs/Flower4.jpeg';
+
+import ThisWarWeAreGoingToWin from './imgs/ThisWarWeAreGoingToWin.jpeg';
+import MyGift from './imgs/MyGift.jpeg';
+import HerGift from './imgs/HerGift.jpeg';
 
 import BlackAndWhiteEmotion from './audios/BlackAndWhite.mp3'
 import OneLove from './audios/OneLove.mp3'
+import CallYourName from './audios/CallYourName.mp3'
+import DarkVogel from './audios/DarkVogel.mp3'
+import TheBreathOfAVow from './audios/TheBreathOfAVow.mp3'
+import Tranquility from './audios/Tranquility.mp3'
+import Freedom from './audios/Freedom.mp3'
 
 export const images = {
     MyLove: MyLove,
@@ -46,14 +77,42 @@ export const images = {
     Turtle: Turtle,
     FirstTurtle: FirstTurtle,
     SecondTurtle: SecondTurtle,
-    TogetherThinkingAboutTheOther: TogetherThinkingAboutTheOther
+    TogetherThinkingAboutTheOther: TogetherThinkingAboutTheOther,
+    Deepdown1: Deepdown1,
+    Deepdown2: Deepdown2,
+    Deepdown3: Deepdown3,
+    SunAndMoon: SunAndMoon,
+    CabyHdp: CabyHdp,
+    DeadOrLive: DeadOrLive,
+    HouseTree: HouseTree,
+    ImOld: ImOld,
+    InstagramMistake: InstagramMistake,
+    SheAuthorizedUsToDestroyTheHouse: SheAuthorizedUsToDestroyTheHouse,
+    SheDoesntThinkTooMuch: SheDoesntThinkTooMuch,
+    TalkingWithHerBrother1: TalkingWithHerBrother1,
+    TalkingWithHerBrother2: TalkingWithHerBrother2,
+    YoWhatTheHell: YoWhatTheHell,
+    Puzzle: Puzzle,
+    ProtectMe1: ProtectMe1,
+    ProtectMe2: ProtectMe2,
+    Flower2: Flower2,
+    Flower3: Flower3,
+    Flower4: Flower4,
+    ThisWarWeAreGoingToWin: ThisWarWeAreGoingToWin,
+    MyGift: MyGift,
+    HerGift: HerGift
 }
 
 export const audios = {
     BlackAndWhiteEmotion: BlackAndWhiteEmotion,
-    OneLove: OneLove
+    OneLove: OneLove,
+    CallYourName: CallYourName,
+    DarkVogel: DarkVogel,
+    TheBreathOfAVow: TheBreathOfAVow,
+    Tranquility: Tranquility,
+    Freedom: Freedom
 }
 
 export const videos = {
-
+    Flower1: Flower1
 }
